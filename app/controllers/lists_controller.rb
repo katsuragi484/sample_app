4f05d9e83917ade04d547e0d1fcc4d33b37f6ba9
katsuragi484 <84887671+katsuragi484@users.noreply.github.com>
2023-01-06 13:50:09 +0000

    Initial Commit

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,48 @@
+class ListsController < ApplicationController
+  def new
+    # Viewへ渡すためのインスタンス変数にからのModelオブジェクトを生成する。
+    @list = List.new
+  end
+
+  def create
+    # データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 詳細画面へリダイレクト
+    flash[:notice] = "投稿が成功しました"
+    redirect_to list_path(list.id)
+  end
+
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to '/lists'
+  end
+
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
+  end
+
+end
